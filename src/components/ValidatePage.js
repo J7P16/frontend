492f1f72f30632b814895441d84a29cf1c3091ae
J7P16 <johncapocyan@gmail.com>
2025-06-10 19:31:14 -0500

    fixed warning error

--- src/components/ValidatePage.js
+++ src/components/ValidatePage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { FiClipboard, FiZap } from 'react-icons/fi';
+import { FiClipboard} from 'react-icons/fi';
 
 const ValidatePage = () => {
   const [message, setMessage] = useState('');
