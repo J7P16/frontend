a6bff102a7fabac7444583bb859bdfb55c321623
VishistS <vishistsantanam@gmail.com>
2025-06-29 15:55:21 -0500

    e

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FiTrendingUp, FiUsers, FiUserCheck, FiZap, FiShield, FiBarChart2, FiUser } from 'react-icons/fi';
 import Navbar from '../reusable/Navbar';
-
+import Footer from '../reusable/Footer';
 
 const Features = () => {
 
   return (
     <>
       <Navbar />
+      <Footer></Footer>
     </>
-
 );
 }
 
